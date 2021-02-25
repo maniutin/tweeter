7a731d56bcd68a085192165fbc4a6694ec64e89b
Nick Maniutin <maniutin@gmail.com>
2021-02-25 04:24:51 +0000

    implemen poorly working page reload after tweet post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   const createTweetElement = function (tweetObj){  
     let createdAt = moment(tweetObj.created_at).fromNow();
@@ -72,22 +48,25 @@ $('.tweet-form').on('submit', function (event) {
   } else {
   $.ajax({url: '/tweets', 
   method: 'POST', 
-  data: $(this).serialize(), 
-  });
+  data: $(this).serialize(),
+  })
+  .then(() => location.reload())
+
+    
   }
  })
 
  const loadTweets = function(){
-  $.ajax({
+    $.ajax({
     url: '/tweets',
     type: "GET",
     dataType: "json",
-  })
+    })
   .then((response) => {
-    renderTweets(response)
-    // console.log(response)
-  });
+    return renderTweets(response)
+  })
   }
+
   loadTweets();
 });
 
